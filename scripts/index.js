0e9ebd9afee6267b8074a2210a79904c83f1c2ba
Niki Fedorov <nikifedorov@mail.ru>
2024-10-20 12:59:47 +0300

    Готовая работа 5ого спринта

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function addCard(dataCard, deleteCard) {
+  const cardNew = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardNew.querySelector('.card__title').textContent = dataCard.name;
+  cardNew.querySelector('.card__image').src = dataCard.link;
+
+  const deleteButton = cardNew.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardNew;
+};
+
 // @todo: Функция удаления карточки
 
+function deleteCard(event) {
+  event.target.closest('.places__item').remove();
+};
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(item => {
+  cardsList.append(addCard(item, deleteCard));
+});
